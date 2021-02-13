3d6efba6f6fa52c108f30ecfc6422d92cdf8e279
Rahul Joshi <rahuljosh777@gmail.com>
2021-02-12 23:50:04 -0500

    Stopped tracking .env

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './index.css';
 function App() {
   return (
     <div className="App">
-      <body className="App-header" style={{background:"rgb(246, 246, 246)"}}>
+      <body style={{background:"rgb(246, 246, 246)"}}>
         <Main />
       </body>
     </div>
